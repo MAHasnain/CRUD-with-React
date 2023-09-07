a19e6cd3ccb530bb875434b73833d0bbf65adfbc
Atta Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-09-07 13:17:47 +0500

    pkgs installed

--- server.mjs
+++ server.mjs
@@ -1,17 +1,42 @@
 import OpenAI from "openai";
 import { PineconeClient } from "@pinecone-database/pinecone";
+import { customAlphabet, nanoid } from "nanoid";
 import express from "express";
+const nanoid = customAlphabet("1234567890", 20);
 import path from "path";
 const __dirname = path.resolve();
+import "dotenv/config.js";
+import Cors from "cors";
+import morgan from "morgan";
 
 const app = express();
-import "dotenv/config.js";
+app.use(express.json());
+app.use(morgan("combined"));
+app.use (Cors());
+
 
 const pinecone = new PineconeClient();
-await pinecone.init ({
+await pinecone.init({
   environment: process.env.PINECONE_ENVIRONMENT,
   apiKey: process.env.PINECONE_API_KEY,
-})
+});
+
+const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+const upsertRequest = {
+  vectors: [
+    {
+      id: nanoid(),
+      values: vector,
+      metadata: {
+        title,
+        body, 
+      },
+    },
+  ],
+  namespace: process.env.PINECONE_NAME_SPACE,
+};
+
+const upsertResponse = await index.upsert({upsertRequest});
 
 
 
@@ -19,6 +44,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 import "./config/index.mjs";
+import morgan from "morgan";
 
 app.get("/", async (req, res) => {
   res.send("Hello World !");
--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,55 @@
-
-import React from 'react'
-import './App';
+import React, { useEffect, useRef, useState } from "react";
+import "./App";
 
 const App = () => {
-  return (
-    <div >
-      <h1 className='main'>Hello M.A.H</h1>
-    </div>
-  )
-}
+  const titleInputRef = useRef(null);
+  const bodyInputRef = useRef(null);
+  const [isloading, setIsloading] = useState(false);
+  const [alert, setAlert] = useState("")
+  
+  useEffect (()=> {
+    setTimeout(()=> {set}, 4000)
+  }, [alert])
+const getWeather = async (event) => {
+  event.preventDefault();
+  
+  try {
+    setIsloading(true)
 
-export default App
+    const response = await axios.get
+  } catch (e) {
+    console.error(e);
+  }
+}
+  
+  return (
+    <div>
+      <h1>Social Stories</h1>
+      <form onSubmit={getWeather}>
+        <label htmlFor="titleInput">Title</label>
+        <input
+          type="text"
+          id="titleInput"
+          maxLength={20}
+          minLength={3}
+          required
+          ref={titleInputRef}
+        />
+        <br />
+        <label htmlFor="BodyInput">Body</label>
+        <textarea
+          type="text"
+          id="bodyInput"
+          maxLength={999}
+          minLength={10}
+          required
+          ref={bodyInputRef}
+        ></textarea>
+        <br />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
